dd7e4bbf8ff252513b4ffd12a975c76a45060691
Lauri Jutila <lauri@sportsetter.com>
2016-06-12 13:51:12 +0300

    PlatformVersionCollection outputs platform specific stats as JSON.

--- lib/hanami_stats/entities/platform_version_collection.rb
+++ lib/hanami_stats/entities/platform_version_collection.rb
@@ -23,13 +23,17 @@ class PlatformVersionCollection
     versions.select(&:semver?)
   end
 
+  def non_semvers
+    versions.reject(&:semver?)
+  end
+
   def major_versions
     semvers.map(&:major).uniq.sort
   end
 
   def counts_per_major_version
     counts = []
-    major_versions.reverse.each do |major|
+    major_versions.each do |major|
       counts.push([major, semvers.select do |version|
         version.major.equal?(major)
       end.map(&:count).inject(&:+)])
@@ -37,10 +41,14 @@ class PlatformVersionCollection
     counts
   end
 
+  def counts_for_other
+    non_semvers.map(&:count).inject(&:+)
+  end
+
   def to_json
     {
+      "other": counts_for_other,
       "total": total_count,
-      "results": counts_per_major_version
-    }.to_json
+    }.merge(counts_per_major_version.to_h).to_json
   end
 end

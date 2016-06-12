1f729e0a795e3a6ab9eda8ea8f74cc1c726be7b1
Lauri Jutila <lauri@sportsetter.com>
2016-06-12 14:35:32 +0300

    Adds specs, cleans up PlatformVersionRepository.

--- lib/hanami_stats/repositories/platform_version_repository.rb
+++ lib/hanami_stats/repositories/platform_version_repository.rb
@@ -1,41 +1,47 @@
 class PlatformVersionRepository
   include Hanami::Repository
 
-  def self.platform(platform)
-    query do
-      where(platform: platform)
+  class << self
+    def platform(platform)
+      query do
+        where(platform: platform)
+      end
     end
-  end
 
-  def self.android
-    query do
-      where(platform: "android")
+    def android
+      query do
+        where(platform: "android")
+      end
     end
-  end
 
-  def self.ios
-    query do
-      where(platform: "ios")
+    def ios
+      query do
+        where(platform: "ios")
+      end
     end
-  end
 
-  def self.windows
-    query do
-      where(platform: "windows-phone")
+    def windows
+      query do
+        where(platform: "windows-phone")
+      end
     end
-  end
 
-  def self.other
-    query do
-      where(platform: "other")
+    def other
+      query do
+        where(platform: "other")
+      end
     end
-  end
 
-  def self.totals
-    {
-      android: PlatformVersionCollection.new(versions: self.android).total_count,
-      ios: PlatformVersionCollection.new(versions: self.ios).total_count,
-      "windows-phone": PlatformVersionCollection.new(versions: self.windows).total_count
-    }
+    def totals
+      {
+        android: collection_for(android).total_count,
+        ios: collection_for(ios).total_count,
+        "windows-phone": collection_for(windows).total_count
+      }
+    end
+
+    def collection_for(versions)
+      PlatformVersionCollection.new(versions: versions)
+    end
   end
 end
--- spec/hanami_stats/repositories/platform_version_repository_spec.rb
+++ spec/hanami_stats/repositories/platform_version_repository_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe PlatformVersionRepository do
   describe "Scopes" do
     describe ".android" do
-      subject { PlatformVersionRepository.android }
+      subject { described_class.android }
 
       it "returns versions for Android platform" do
         expect(subject.all).to be_instance_of Array
@@ -14,7 +14,7 @@ RSpec.describe PlatformVersionRepository do
     end
 
     describe ".ios" do
-      subject { PlatformVersionRepository.ios }
+      subject { described_class.ios }
 
       it "returns versions for iOS platform" do
         expect(subject.all).to be_instance_of Array
@@ -25,7 +25,7 @@ RSpec.describe PlatformVersionRepository do
     end
 
     describe ".ios" do
-      subject { PlatformVersionRepository.windows }
+      subject { described_class.windows }
 
       it "returns versions for Windows Phone platform" do
         expect(subject.all).to be_instance_of Array
@@ -36,13 +36,63 @@ RSpec.describe PlatformVersionRepository do
     end
 
     describe ".other" do
-      subject { PlatformVersionRepository.other }
+      subject { described_class.other }
 
       it "returns all other platform versions" do
         expect(subject.all).to be_instance_of Array
         subject.all.each do |result|
           expect(result.platform).to eq "other"
         end
+      end
+    end
+
+    describe ".platform(platform)" do
+      context "Known platform" do
+        subject { described_class.platform("ios") }
+
+        it "returns versions for given platform" do
+          expect(subject.all).to be_instance_of Array
+          subject.all.each do |result|
+            expect(result.platform).to eq "ios"
+          end
+        end
+      end
+
+      context "Unknown platform" do
+        subject { described_class.platform("foo") }
+
+        it "returns no results for platforms that don't exist in the Repository" do
+          expect(subject.all).to be_instance_of Array
+          expect(subject.all).to be_empty
+        end
+      end
+    end
+  end
+
+  describe ".totals" do
+    subject { described_class.totals }
+
+    it "returns a hash of results" do
+      expect(subject).to be_instance_of Hash
+      expect(subject).to have_key(:android)
+      expect(subject).to have_key(:ios)
+      expect(subject).to have_key(:"windows-phone")
+    end
+
+    describe "Results" do
+      before do
+        versions = [
+          FactoryGirl.build_list(:platform_version, 10, :android),
+          FactoryGirl.build_list(:platform_version, 20, :ios),
+          FactoryGirl.build_list(:platform_version, 30, :windows)
+        ]
+        versions.flatten.each { |version| described_class.create(version) }
+      end
+
+      it "returns the totals for each platform" do
+        expect(subject[:android]).to be_a Integer
+        expect(subject[:ios]).to be_a Integer
+        expect(subject[:"windows-phone"]).to be_a Integer
       end
     end
   end
